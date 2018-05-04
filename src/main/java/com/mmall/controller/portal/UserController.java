e7b9d32ff95e44cc392df33989433d27190da174
zhaoliguo3 <2284777094@qq.com>
2018-05-04 11:42:49 +0800

    'mmall电商后台'

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
      * @param session
      * @return
      */
-    @RequestMapping(value = "login.do")
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session){
         ServerResponse<User> response = iUserService.login(username,password);
