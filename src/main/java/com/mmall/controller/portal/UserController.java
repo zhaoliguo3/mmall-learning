e175db6d4f44a6fc5d7d10f67d099c40c0fcbda4
zhaoliguo3 <2284777094@qq.com>
2018-05-04 11:41:33 +0800

    '修改UserController'

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
      * @param session
      * @return
      */
-    @RequestMapping(value = "login.do",method = RequestMethod.POST)
+    @RequestMapping(value = "login.do")
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session){
         ServerResponse<User> response = iUserService.login(username,password);
